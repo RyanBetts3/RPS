e870cd479ebe1411cadb10d1095b7620f4fdffc1
RyanBetts3 <68836758+RyanBetts3@users.noreply.github.com>
2023-06-07 21:43:42 +1000

    First commit

---
+++ styles.cs
@@ -0,0 +1,14 @@
+body {
+    background-color: #f4f4f4;
+}
+
+.choices img {
+    width: 100px;
+    margin: 20px;
+    cursor: pointer;
+}
+
+#score, #result {
+    font-size: 24px;
+    font-weight: bold;
+}
